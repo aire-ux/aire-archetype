6664abfd7a7a86e13ba3037e257ec689074067ce
josiah <j@groovv.co>
2022-10-11 17:15:50 -0500

    Adding component-tests and additional root files

---
+++ src/main/resources/archetype-resources/src/test/typescript/harness.ts
@@ -0,0 +1,17 @@
+import {
+  ${component-name.substring(0,1).toUpperCase()}${component-name.substring(1)}
+} from "@${organization}/${component-name}";
+
+export default function setup() {
+
+
+  window.customElements.define(
+      '${component-name}',
+      ${component-name.substring(0,1).toUpperCase()}${component-name.substring(1)});
+  Object.defineProperty(window.location, 'href', {
+    writable: true,
+    value: 'https://localhost'
+  });
+  // (window as any).chai.use(chaiDomDiff);
+}
+setup();
