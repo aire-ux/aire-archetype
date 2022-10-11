e5fd6f20f507a81f6c88854dcd91581f8463b33c
josiah <j@groovv.co>
2022-10-11 18:39:19 -0500

    Fixing some string issues

---
+++ src/main/resources/archetype-resources/src/main/java/__component-type-name__.java
@@ -0,0 +1,18 @@
+package ${package};
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.Button;
+
+@Tag("${component-name}")
+@JsModule("@${organization}/${component-name}/${component-name}")
+@CssImport("@${organization}/${component-name}/styles/${component-name}.css")
+public class ${component-type-name} extends HtmlContainer {
+
+  public ${component-type-name} {
+    add(new Button("Hello from ${component-type-name}!"));
+  }
+
+}
